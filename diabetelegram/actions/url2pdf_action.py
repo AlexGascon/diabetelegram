f1db717c6e71a09ed86d6143f623224670f738d6
Alex Gascón Bononad <alex@macbook-pro-de-alex.home>
2023-05-01 10:47:14 +0200

    URL2PDF Action
    Creating an action that given a URL, will convert it to PDF and send it to the user
    
    It will use a service created separately, URL2PDF.

---
+++ diabetelegram/actions/url2pdf_action.py
@@ -0,0 +1,35 @@
+import logging
+import os
+import re
+import requests
+from urllib.parse import urlparse, urlunparse
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+
+class Url2PdfAction(BaseAction):
+    # Matches messages starting with 'http://' or 'https://'
+    URL_REGEX = r"^(https?://).*"
+
+    def matches(self):
+        return bool(re.match(self.URL_REGEX, self.message_text.lower()))
+
+    def handle(self):
+        url2pdf_endpoint = os.environ['URL2PDF_ENDPOINT']
+        requested_url = self.message_text
+        parsed = urlparse(requested_url)
+
+        url = urlunparse(
+            parsed._replace(
+                scheme='https',
+                netloc=url2pdf_endpoint,
+                path='',
+                params='',
+                query='url=' + parsed.netloc + parsed.path
+            )
+        )
+
+        # We can use a URL with send_document, and Telegram will get the file and
+        # send it automatically
+        self.telegram.send_document(self.message, url)
